d02cf9cd61cf3cb30888c1f01593ffca666425bf
Clément Dubrisay <clement.dubrisay@gmail.com>
2018-05-25 15:55:29 +0200

    push push

--- script.js
+++ script.js
@@ -1,5 +1,28 @@
 $(document).ready(function(){
     
+    $('.thumb1').click(function(){
+        $('.thumb').removeClass('on').addClass('off');
+        $(this).removeClass('off').addClass('on');
+        $('.img1').animate({'opacity':'1'}, 250);
+        $('.img2').animate({'opacity':'0'}, 250);
+        $('.img3').animate({'opacity':'0'}, 250);
+    });
+
+    $('.thumb2').click(function(){
+        $('.thumb').removeClass('on').addClass('off');
+        $(this).removeClass('off').addClass('on');
+        $('.img1').animate({'opacity':'0'}, 250);
+        $('.img2').animate({'opacity':'1'}, 250);
+        $('.img3').animate({'opacity':'0'}, 250);
+    });
+
+    $('.thumb3').click(function(){
+        $('.thumb').removeClass('on').addClass('off');
+        $(this).removeClass('off').addClass('on');
+        $('.img1').animate({'opacity':'0'}, 250);
+        $('.img2').animate({'opacity':'0'}, 250);
+        $('.img3').animate({'opacity':'1'}, 250);
+    });
 
 
 
@@ -7,6 +30,4 @@ $(document).ready(function(){
 
 
 
-
-    
 });
